b9a672537a3304c0804df7155fba53b9bfe3fc8c
nhtetlinn <htetlinn7989@gmail.com>
2022-09-29 17:15:29 +0630

    Lvl3

---
+++ myapp-with-router/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header title="About Page" subtitle="Say Something"/>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/header.vue";
+export default{
+  name:'AboutView',
+  components: {Header}
+}
+</script>
